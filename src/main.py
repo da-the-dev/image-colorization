fceed6c4bad12bcf9f679b223e59cbbb9c644ee7
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-10-23 21:57:23 +0300

    Formatting

--- src/main.py
+++ src/main.py
@@ -5,26 +5,25 @@ from src.transform_data import transform_GAN_data
 from src.model import GNet, GAN_Model
 import torch
 
+
 def run_default_GAN():
-    train_paths, val_paths = get_data(num=1000)
+    train_paths, val_paths = get_data(num=100)
 
     train_dl = transform_GAN_data(train_paths, split="train", batch_size=16)
     val_dl = transform_GAN_data(val_paths, split="val", batch_size=16)
 
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     model_G = GNet(device, optimizer="Adam", body="resnet34")
 
     model_G.pretrain(train_dl, epochs=5)
 
-
-    GAN_model = GAN_Model(model_G.G_net, lr_G=0.0004, lr_D=0.0004, beta1=0.5, beta2=0.999, lamda=100.)
-    GAN_model.train_model(train_dl, epochs=5)
-
-
     model_G.save_model(path=os.path.join(os.getcwd(), "models/gan_g_net.pt"))
 
-    GAN_model.train_model(train_dl, epochs=5, display_every=50)
+    GAN_model = GAN_Model(
+        model_G.G_net, lr_G=0.0004, lr_D=0.0004, beta1=0.5, beta2=0.999, lamda=100.0
+    )
+    GAN_model.train_model(train_dl, epochs=5)
 
     GAN_model.save_model(path=os.path.join(os.getcwd(), "models/gan.pt"))
 
@@ -35,4 +34,5 @@ def main(archetecture):
         run_default_GAN()
 
 
-main("GAN")
+if __name__ == "__main__":
+    main("GAN")
--- src/model.py
+++ src/model.py
@@ -10,6 +10,7 @@ from src.utils import *
 
 # Class for initializing Generator model
 
+
 class GNet:
     def __init__(self, device, G_net=None, optimizer="Adam", body="resnet34"):
         self.device = device
@@ -22,8 +23,7 @@ class GNet:
         if optimizer == "Adam":
             self.optimizer = optim.Adam(self.G_net.parameters(), lr=0.0004)
 
-        self.criterion = nn.L1Loss()  
-
+        self.criterion = nn.L1Loss()
 
     def build_G_net(self, n_input=1, n_output=2, size=256, body="resnet34"):
         if body == "resnet34":
@@ -31,26 +31,25 @@ class GNet:
         backbone = create_body(body_model, pretrained=True, n_in=n_input, cut=-2)
         G_net = DynamicUnet(backbone, n_output, (size, size)).to(self.device)
         return G_net
-    
+
     def pretrain(self, train_dl, epochs):
         for itr in range(epochs):
             loss_meter = AverageMeter()
             for data in tqdm(train_dl):
-                L, ab = data['L'].to(self.device), data['ab'].to(self.device)
+                L, ab = data["L"].to(self.device), data["ab"].to(self.device)
                 preds = self.G_net(L)
                 loss = self.criterion(preds, ab)
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-                
+
                 loss_meter.update(loss.item(), L.size(0))
-                
+
             print(f"Epoch {itr + 1}/{epochs}")
             print(f"L1 Loss: {loss_meter.avg:.5f}")
 
     def get_model(self):
         return self.G_net
-    
 
     def save_model(self, path="generator.pt"):
         torch.save(self.G_net.state_dict(), path)
@@ -59,82 +58,98 @@ class GNet:
         self.G_net.load_state_dict(torch.load(path), map_location=self.device)
 
 
-
 # GAN class implementation
 
-#GAN Loss
+# GAN Loss
+
 
 class GANLoss(nn.Module):
-    def __init__(self, gan_mode='vanilla', real_label=0.9, fake_label=0.1):
+    def __init__(self, gan_mode="vanilla", real_label=0.9, fake_label=0.1):
         super().__init__()
-        self.register_buffer('real_label', torch.tensor(real_label))
-        self.register_buffer('fake_label', torch.tensor(fake_label))
-        if gan_mode == 'vanilla':
+        self.register_buffer("real_label", torch.tensor(real_label))
+        self.register_buffer("fake_label", torch.tensor(fake_label))
+        if gan_mode == "vanilla":
             self.loss = nn.BCEWithLogitsLoss()
-        elif gan_mode == 'lsgan':
+        elif gan_mode == "lsgan":
             self.loss = nn.MSELoss()
-    
+
     def get_labels(self, preds, target_is_real):
         if target_is_real:
             labels = self.real_label
         else:
             labels = self.fake_label
         return labels.expand_as(preds)
-    
+
     def __call__(self, preds, target_is_real):
         labels = self.get_labels(preds, target_is_real)
         loss = self.loss(preds, labels)
         return loss
-    
+
 
 # Discriminator
+
 
 class Discriminator(nn.Module):
     def __init__(self, input_c, num_filters=64, n_down=3):
         super().__init__()
         model = [self.get_layers(input_c, num_filters, norm=False)]
-        model += [self.get_layers(num_filters * 2 ** i, num_filters * 2 ** (i + 1), s=1 if i == (n_down-1) else 2) 
-                          for i in range(n_down)] 
-        model += [self.get_layers(num_filters * 2 ** n_down, 1, s=1, norm=False, act=False)] 
-        self.model = nn.Sequential(*model)                                                   
-        
-    def get_layers(self, ni, nf, k=4, s=2, p=1, norm=True, act=True): 
-        layers = [nn.Conv2d(ni, nf, k, s, p, bias=not norm)]          
-        if norm: layers += [nn.BatchNorm2d(nf)]
-        if act: layers += [nn.LeakyReLU(0.2, True)]
+        model += [
+            self.get_layers(
+                num_filters * 2**i,
+                num_filters * 2 ** (i + 1),
+                s=1 if i == (n_down - 1) else 2,
+            )
+            for i in range(n_down)
+        ]
+        model += [
+            self.get_layers(num_filters * 2**n_down, 1, s=1, norm=False, act=False)
+        ]
+        self.model = nn.Sequential(*model)
+
+    def get_layers(self, ni, nf, k=4, s=2, p=1, norm=True, act=True):
+        layers = [nn.Conv2d(ni, nf, k, s, p, bias=not norm)]
+        if norm:
+            layers += [nn.BatchNorm2d(nf)]
+        if act:
+            layers += [nn.LeakyReLU(0.2, True)]
         return nn.Sequential(*layers)
-    
+
     def forward(self, x):
         return self.model(x)
-    
+
 
 # GAN Model
 
+
 class GAN_Model(nn.Module):
-    def __init__(self, G_net, lr_G=0.0004, lr_D=0.0004, beta1=0.5, beta2=0.999, lamda=100.):
+    def __init__(
+        self, G_net, lr_G=0.0004, lr_D=0.0004, beta1=0.5, beta2=0.999, lamda=100.0
+    ):
         super().__init__()
-        
+
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.lamda = lamda
-        
+
         self.G_net = G_net.to(self.device)
-        self.D_net = init_model(Discriminator(input_c=3, n_down=3, num_filters=64), self.device)
-        self.GANcriterion = GANLoss(gan_mode='vanilla').to(self.device)
+        self.D_net = init_model(
+            Discriminator(input_c=3, n_down=3, num_filters=64), self.device
+        )
+        self.GANcriterion = GANLoss(gan_mode="vanilla").to(self.device)
         self.L1criterion = nn.L1Loss()
         self.opt_G = optim.Adam(self.G_net.parameters(), lr=lr_G, betas=(beta1, beta2))
         self.opt_D = optim.Adam(self.D_net.parameters(), lr=lr_D, betas=(beta1, beta2))
-    
+
     def set_requires_grad(self, model, requires_grad=True):
         for p in model.parameters():
             p.requires_grad = requires_grad
-        
+
     def setup_input(self, data):
-        self.L = data['L'].to(self.device)
-        self.ab = data['ab'].to(self.device)
-        
+        self.L = data["L"].to(self.device)
+        self.ab = data["ab"].to(self.device)
+
     def forward(self):
         self.fake_color = self.G_net(self.L)
-    
+
     def backward_D(self):
         fake_image = torch.cat([self.L, self.fake_color], dim=1)
         fake_preds = self.D_net(fake_image.detach())
@@ -144,7 +159,7 @@ class GAN_Model(nn.Module):
         self.loss_D_real = self.GANcriterion(real_preds, True)
         self.loss_D = (self.loss_D_fake + self.loss_D_real) * 0.5
         self.loss_D.backward()
-    
+
     def backward_G(self):
         fake_image = torch.cat([self.L, self.fake_color], dim=1)
         fake_preds = self.D_net(fake_image)
@@ -152,7 +167,7 @@ class GAN_Model(nn.Module):
         self.loss_G_L1 = self.L1criterion(self.fake_color, self.ab) * self.lamda
         self.loss_G = self.loss_G_GAN + self.loss_G_L1
         self.loss_G.backward()
-    
+
     def optimize(self):
         self.forward()
         self.D_net.train()
@@ -160,45 +175,28 @@ class GAN_Model(nn.Module):
         self.opt_D.zero_grad()
         self.backward_D()
         self.opt_D.step()
-        
+
         self.G_net.train()
         self.set_requires_grad(self.D_net, False)
         self.opt_G.zero_grad()
         self.backward_G()
         self.opt_G.step()
 
-
     def train_model(self, train_dl, epochs, display_every=200):
         # пока что ток трейн (лосс на трейне тоже не плохо)
-        for itr in range(epochs):
-            loss_meter_dict = create_loss_meters() 
-            i = 0                                  
-            for data in tqdm(train_dl):
-                self.setup_input(data) 
+        for epoch in range(epochs):
+            loss_meter_dict = create_loss_meters()
+
+            for i, data in enumerate(tqdm(train_dl)):
+                self.setup_input(data)
                 self.optimize()
-                update_losses(self, loss_meter_dict, count=data['L'].size(0)) 
-                i += 1
+                update_losses(self, loss_meter_dict, count=data["L"].size(0))
+
                 if i % display_every == 0:
-                    print(f"\nEpoch {itr+1}/{epochs}")
+                    print(f"\nEpoch {epoch+1}/{epochs}")
                     print(f"Iteration {i}/{len(train_dl)}")
-                    log_results(loss_meter_dict) 
-                    visualize(self, data, save=False) 
+                    log_results(loss_meter_dict)
+                    visualize(self, data, save=True)
 
     def save_model(self, path="model.pt"):
         torch.save(self.state_dict(), path)
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
