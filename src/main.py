5982d1e9363de66234f03944d0fa132a2cb52123
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-11-03 17:58:17 +0300

    Added types

--- src/main.py
+++ src/main.py
@@ -4,11 +4,12 @@
 # сюда можно докинуть hydra и юзать как параметр название архитектуры
 
 import hydra
+from omegaconf import DictConfig
 from src.arch.cgan.runner import runner_cgan
 
 
 @hydra.main(version_base=None, config_path="../configs", config_name="main")
-def main(cfg):
+def main(cfg: DictConfig):
     if cfg.model.arch == "CGAN":
         runner_cgan(cfg)
 
