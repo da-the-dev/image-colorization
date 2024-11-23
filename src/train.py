7d39dd22ecadf51b0924294f7ff0a39b4036f586
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-11-23 20:47:59 +0300

    Added model logging

--- src/train.py
+++ src/train.py
@@ -9,7 +9,10 @@ from src.arch.proper_cgan.dataset import GAN_Dataset
 from src.arch.proper_cgan.model import GAN, Generator
 
 # Enable autologging
-mlflow.pytorch.autolog(checkpoint=False)
+mlflow.pytorch.autolog(
+    checkpoint=False, # Skip checkpoining, no metrics, no need to save this info
+    log_models=False, # Skip logging models, we do it manually
+)
 
 
 @hydra.main(version_base=None, config_path="../configs", config_name="main")
@@ -30,18 +33,36 @@ def train(cfg: DictConfig):
     with mlflow.start_run() as run:
         # Take first batch from dataloader for test images
         test_images = next(iter(train_loader))
-        
+
+        input_example = (
+            test_images[0, 0, :, :].unsqueeze(0).unsqueeze(0).detach().cpu().numpy()
+        )
+
         G_net = Generator(test_images)
         print("Started generator pretrain...")
         pretrainer = pl.Trainer(max_epochs=cfg.model.pretrain_epochs)
         pretrainer.fit(G_net, train_loader)
         print("Generator pretrain completed!")
 
+        mlflow.pytorch.log_model(
+            pytorch_model=G_net,
+            artifact_path="gnet",
+            input_example=input_example,
+            registered_model_name="U-net Generator (ResNet backbone)",
+        )
+
         print("Started GAN training...")
         GAN_model = GAN(G_net, test_images)
         trainer = pl.Trainer(max_epochs=cfg.model.epochs)
         trainer.fit(GAN_model, train_loader)
         print("GAN train completed!")
+
+        mlflow.pytorch.log_model(
+            pytorch_model=GAN_model,
+            artifact_path="gan",
+            input_example=input_example,
+            registered_model_name="Conditional GAN",
+        )
 
 
 if __name__ == "__main__":
