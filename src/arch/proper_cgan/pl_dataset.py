49dd67f064d5cc3e3bc68efb63c20f87765b1e97
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-11-24 18:09:47 +0300

    lint: Formatting

--- src/arch/proper_cgan/pl_dataset.py
+++ src/arch/proper_cgan/pl_dataset.py
@@ -13,7 +13,6 @@ from src.arch.proper_cgan.dataset import GAN_Dataset
 import pytorch_lightning as pl
 
 
-
 class GanDataModule(pl.LightningDataModule):
     def __init__(self, data_dir_train, data_dir_test, batch_size, num_workers):
         super().__init__()
@@ -22,20 +21,37 @@ class GanDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.num_workers = num_workers
 
-
-    def setup(self, stage): # https://youtu.be/NjF1ZpRO4Ws?si=vmrpGVxMMUIt6dhC 4:00 что делать с кастомным датасетом
+    def setup(
+        self, stage
+    ):  # https://youtu.be/NjF1ZpRO4Ws?si=vmrpGVxMMUIt6dhC 4:00 что делать с кастомным датасетом
         entire_dataset = GAN_Dataset(self.data_dir_train)
-        
+
         self.train_ds, self.val_ds = random_split(entire_dataset, [0.8, 0.2])
 
         self.test_ds = GAN_Dataset(self.data_dir_test)
 
     def train_dataloader(self):
-        return DataLoader(self.train_ds, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True, pin_memory=True)
-        
+        return DataLoader(
+            self.train_ds,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            pin_memory=True,
+        )
 
     def val_dataloader(self):
-        return DataLoader(self.val_ds, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False, pin_memory=True)
+        return DataLoader(
+            self.val_ds,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            pin_memory=True,
+        )
 
     def test_dataloader(self):
-        return DataLoader(self.test_ds, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False)
+        return DataLoader(
+            self.test_ds,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+        )
